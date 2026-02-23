813e5ea210fb9815541d759cbe2068dd150b0412
Alexey Chistov <alexey.chistov10@gmail.com>
2026-02-23 12:40:41 +0300

    fix

--- 3822B1FI1/3_acc_jacobi_oneapi/chistov_alexey/acc_jacobi_oneapi.cpp
+++ 3822B1FI1/3_acc_jacobi_oneapi/chistov_alexey/acc_jacobi_oneapi.cpp
@@ -1,4 +1,4 @@
-#include "jacobi_acc_oneapi.h"
+#include "acc_jacobi_oneapi.h"
 #include <algorithm>
 #include <buffer.hpp>
 #include <handler.hpp>
@@ -11,27 +11,27 @@ std::vector<float> JacobiAccONEAPI(const std::vector<float> a,
                                    float accuracy,
                                    sycl::device device) {
   int size = b.size();
-  std::vector<float> curr_ans(size, 0.0f);
-  std::vector<float> prev_ans(size, 0.0f);
+  std::vector<float> current_solution(size, 0.0f);
+  std::vector<float> previous_solution(size, 0.0f);
   float max_diff = 0.0f;
   int iteration = 0;
 
   {
-    sycl::buffer<float> buf_a(a.data(), a.size());
-    sycl::buffer<float> buf_b(b.data(), b.size());
-    sycl::buffer<float> buf_curr(curr_ans.data(), curr_ans.size());
-    sycl::buffer<float> buf_prev(prev_ans.data(), prev_ans.size());
-    sycl::buffer<float> buf_max_diff(&max_diff, 1);
+    sycl::buffer<float> matrix_buffer(a.data(), a.size());
+    sycl::buffer<float> vector_b_buffer(b.data(), b.size());
+    sycl::buffer<float> buf_curr(current_solution.data(), current_solution.size());
+    sycl::buffer<float> previous_solution_buffer(previous_solution.data(), previous_solution.size());
+    sycl::buffer<float> max_diff_buffer(&max_diff, 1);
     sycl::queue queue(device);
 
     while (iteration++ < ITERATIONS) {
       queue.submit([&](sycl::handler &cgh) {
-        auto a_access = buf_a.get_access<sycl::access::mode::read>(cgh);
-        auto b_access = buf_b.get_access<sycl::access::mode::read>(cgh);
-        auto prev_access = buf_prev.get_access<sycl::access::mode::read>(cgh);
+        auto a_access = matrix_buffer.get_access<sycl::access::mode::read>(cgh);
+        auto b_access = vector_b_buffer.get_access<sycl::access::mode::read>(cgh);
+        auto prev_access = previous_solution_buffer.get_access<sycl::access::mode::read>(cgh);
         auto curr_access = buf_curr.get_access<sycl::access::mode::write>(cgh);
         
-        auto reduction = sycl::reduction(buf_max_diff, cgh, sycl::maximum<float>());
+        auto reduction = sycl::reduction(max_diff_buffer, cgh, sycl::maximum<float>());
 
         cgh.parallel_for(sycl::range<1>(size), reduction,
                          [=](sycl::id<1> idx, auto &max_diff_reduction) {
@@ -54,14 +54,14 @@ std::vector<float> JacobiAccONEAPI(const std::vector<float> a,
 
       queue.wait();
 
-      float current_max_diff = buf_max_diff.get_host_access()[0];
+      float current_max_diff = max_diff_buffer.get_host_access()[0];
       if (current_max_diff < accuracy) {
         break;
       }
 
       queue.submit([&](sycl::handler &cgh) {
         auto curr_access = buf_curr.get_access<sycl::access::mode::read>(cgh);
-        auto prev_access = buf_prev.get_access<sycl::access::mode::write>(cgh);
+        auto prev_access = previous_solution_buffer.get_access<sycl::access::mode::write>(cgh);
         
         cgh.parallel_for(sycl::range<1>(size), [=](sycl::id<1> idx) {
           prev_access[idx] = curr_access[idx];
@@ -72,5 +72,5 @@ std::vector<float> JacobiAccONEAPI(const std::vector<float> a,
     }
   }
 
-  return curr_ans;
+  return current_solution;
 }
