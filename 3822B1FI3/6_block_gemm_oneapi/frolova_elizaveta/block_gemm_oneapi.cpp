0ff6ad41a2b58366b950e766b2fb3d2fba6299e7
ElizavetaFrolova <liza1827frol@gmail.com>
2026-03-12 14:08:38 +0300

    fix block matrix multiplication

--- 3822B1FI3/6_block_gemm_oneapi/frolova_elizaveta/block_gemm_oneapi.cpp
+++ 3822B1FI3/6_block_gemm_oneapi/frolova_elizaveta/block_gemm_oneapi.cpp
@@ -28,50 +28,52 @@ std::vector<float> GemmBlockONEAPI(
         queue.memset(c_dev, 0, size * size * sizeof(float));
         queue.wait();
         
-        const size_t wg_size = 16;
-        
         queue.submit([&](sycl::handler& cgh) {
             sycl::local_accessor<float, 2> a_block(sycl::range<2>(BLOCK_SIZE, BLOCK_SIZE), cgh);
             sycl::local_accessor<float, 2> b_block(sycl::range<2>(BLOCK_SIZE, BLOCK_SIZE), cgh);
             
             cgh.parallel_for(sycl::nd_range<2>(
                 sycl::range<2>(size, size),
-                sycl::range<2>(wg_size, wg_size)),
+                sycl::range<2>(BLOCK_SIZE, BLOCK_SIZE)),
                 [=](sycl::nd_item<2> item) {
                 
-                size_t i = item.get_global_id(0);
-                size_t j = item.get_global_id(1);
+                size_t global_row = item.get_global_id(0);
+                size_t global_col = item.get_global_id(1);
                 
-                if (i >= size || j >= size) return;
+                if (global_row >= size || global_col >= size) return;
                 
-                size_t bi = i / BLOCK_SIZE;
-                size_t bj = j / BLOCK_SIZE;
-                size_t li = i % BLOCK_SIZE;
-                size_t lj = j % BLOCK_SIZE;
+                size_t local_row = item.get_local_id(0);
+                size_t local_col = item.get_local_id(1);
                 
-                double sum = 0.0;
+                size_t block_row = global_row / BLOCK_SIZE;
+                size_t block_col = global_col / BLOCK_SIZE;
                 
-                for (size_t bk = 0; bk < num_blocks; ++bk) {
+                float sum = 0.0f;
+                
+                for (size_t block_k = 0; block_k < num_blocks; ++block_k) {
                     
-                    a_block[li][lj] = a_dev[(bi * BLOCK_SIZE + li) * size + (bk * BLOCK_SIZE + lj)];
-                    b_block[li][lj] = b_dev[(bk * BLOCK_SIZE + li) * size + (bj * BLOCK_SIZE + lj)];
+                    size_t a_global_row = block_row * BLOCK_SIZE + local_row;
+                    size_t a_global_col = block_k * BLOCK_SIZE + local_col;
+                    a_block[local_row][local_col] = a_dev[a_global_row * size + a_global_col];
+                    
+                    size_t b_global_row = block_k * BLOCK_SIZE + local_row;
+                    size_t b_global_col = block_col * BLOCK_SIZE + local_col;
+                    b_block[local_row][local_col] = b_dev[b_global_row * size + b_global_col];
                     
                     item.barrier(sycl::access::fence_space::local_space);
                     
                     for (size_t k = 0; k < BLOCK_SIZE; ++k) {
-                        sum += static_cast<double>(a_block[li][k]) * 
-                               static_cast<double>(b_block[k][lj]);
+                        sum += a_block[local_row][k] * b_block[k][local_col];
                     }
                     
                     item.barrier(sycl::access::fence_space::local_space);
                 }
                 
-                c_dev[i * size + j] = static_cast<float>(sum);
+                c_dev[global_row * size + global_col] = sum;
             });
         });
         
         queue.wait();
-        
         queue.memcpy(c.data(), c_dev, size * size * sizeof(float)).wait();
         
         sycl::free(a_dev, queue);
